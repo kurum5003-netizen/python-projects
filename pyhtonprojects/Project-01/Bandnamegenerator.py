cb59f7a2dbe9aaeef2e7b390d0d1fd66d485430f
Berk Kurum <kurum5003@gmail.com>
2026-07-01 17:29:03 +0300

    Add files via upload

---
+++ pyhtonprojects/Project-01/Bandnamegenerator.py
@@ -0,0 +1,11 @@
+#input-output-basic-project
+print("Welcom to the Band Name Generator.")
+city= input("Which city did you grow up in?")
+pet= input("What is the name of a pet")
+print("Your band name could be:" + city +" " + pet)
+print(len(city))
+print(len(pet))
+temp=city
+city=pet
+pet=temp
+print(city +" " + pet)
