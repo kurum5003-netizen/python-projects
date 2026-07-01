1c26c62c35fcd115d040f45dc47e0ac7e8d9cec5
Berk Kurum <kurum5003@gmail.com>
2026-07-01 22:42:22 +0300

    Update TipCalculator.py

--- Projects/project-02/TipCalculator.py
+++ Projects/project-02/TipCalculator.py
@@ -1,1 +1,7 @@
-
+print("Welcome to the trip calculator!")
+bill=float(input("what was the total bill?"))
+tip=int(input("how much tip would you like to give? 10 , 12 or 15"))
+people=int(input("How many people to split the bill?"))
+a=(bill*tip)/100
+b=(a+bill)/people
+print(f"Each person should pay $ {round(b,3)}")
