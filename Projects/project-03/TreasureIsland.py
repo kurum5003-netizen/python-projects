7b419c2768e54b0565c524c40ad2f539f9183b40
Berk Kurum <kurum5003@gmail.com>
2026-07-03 14:19:25 +0300

    Update TreasureIsland.py

--- Projects/project-03/TreasureIsland.py
+++ Projects/project-03/TreasureIsland.py
@@ -1,14 +1,14 @@
 print("welcome to treasure ısland.\n your miison is find to trasure")
 yon1=input("left or right")
-if(yon1=="left"):
+if yon1=="left":
     yon2=input("swim or wait")
-    if(yon2=="wait"):
-         yonb3=input("which door(red/blue/yellow")
-         if(yonb3=="yellow"):
+    if yon2=="wait":
+         yonb3=input('which "door"(red/blue/yellow')
+         if yonb3=="yellow":
              print("you win")
-         elif(yonb3=="red"):
+         elif yonb3=="red":
              print("you lose")
-         elif(yonb3=="blue"):
+         elif yonb3=="blue":
              print("you lose")
          else:
              print("geçersiz seçim")
