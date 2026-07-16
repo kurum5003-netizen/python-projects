3e069e0df7c6b9d116785a5f8e9e855df4be1d27
Berk Kurum <kurum5003@gmail.com>
2026-07-16 04:19:20 +0300

    Update numberguessing.py

--- Projects/project-13/numberguessing.py
+++ Projects/project-13/numberguessing.py
@@ -1,1 +1,46 @@
+import random
+
+HARD_LIFE=5
+EASY_LIFE=10
+
+def set_difficulty():
+    while True:
+        choice = input("zor mod için 'z' ye kolay mod  için 'e' basın")
+        if choice=="z":
+            return HARD_LIFE
+        elif choice=="e":
+            return EASY_LIFE
+        else:
+            print("lütfen geçerli karekter girin")
+
+
+def game(mode,number):
+    if mode==5:
+        print("zor modasınız can hakkınız:5")
+    else:
+        print("kolay modasınız can hakkınız:10")
+    while mode>0:
+        tahmin=int(input("tahmininiz nedir?"))
+        if tahmin>number:
+            print("tahmininiz yukarda")
+            mode-=1
+            print(f"kalan can:{mode}")
+        elif tahmin<number:
+            print("tahmininiz aşağıda")
+            mode-=1
+            print(f"kalan can:{mode}")
+        else:
+            print("TEBRİKLER!!! ")
+            return
+
+    print(f"Canınız bitti. Doğru sayı {number} idi.")
+
+print("Sayı tahmin oyununa hoşgeldiniz!!")
+numrandom=random.randint(1,100)
+mode_choice=set_difficulty()
+game(mode_choice,numrandom)
+
+
+
+
 
